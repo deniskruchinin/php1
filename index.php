6a8a638a2946baa81a4ee3716bf9814efaa5f9d9
den <mega-tresh@mail.ru>
2020-08-03 07:03:19 +0300

    4

--- index.php
+++ index.php
@@ -1,78 +1,24 @@
+<div style="width: 70%; margin:auto ">
+
+    <?php
+    $files = scandir("img");
+    for ($i = 2; $i < count($files); $i++) : ?>
+        <?php
+        if ($i % 2 == 0) {
+            echo "<br>";
+        }
+        ?>
+        <img style="width: 20%;" src="img/<?= $files[$i] ?>">
+    <?php
+    endfor;
+    ?>
+</div>
+
+
+
 <?php
-//задание 1 
 
-$a = rand(-10, 10);
-$b =  rand(-10, 10);
-$c = null;
-
-if ($a >= 0 && $b >= 0) {
-    $c = "-";
-    echo "$a" . "$c" . "$b" . "=" . ($a - $b);
-} elseif ($a < 0 && $b < 0) {
-    $c = "*";
-    echo "$a" . "$c" . "$b" . "=" . ($a * $b);
-} elseif ($a >= 0 && $b < 0 || $a < 0 && $b >= 0) {
-    $c = "+";
-    echo "($a)" . "$c" . "($b)" . "=" . ($a + $b);
-};
-
-
-echo "<br/>" . "задание 2" . "<br/>";;
-//задание 2 
-
-function f($n, $Num)
-{
-    echo " " . "$n";
-    if ($n >= $Num) return;
-    f($n + 1, $Num);
-}
-f(0, 15);
-
-
-echo "<br/>" . "задание 3" . "<br/>";
-//задание 3
-
-function sum($a, $b)
-{
-    return $a + $b;
-};
-function subtraction($a, $b)
-{
-    return $a - $b;
-};
-function multiplication($a, $b)
-{
-    return $a * $b;
-};
-function division($a, $b)
-{
-    return $a / $b;
-};
-echo sum(2,2);
-
-
-echo "<br/>" . "задание 4" . "<br/>";
-//задание 4
-
-function arithmetic($arg1, $arg2, $operation)
-{
-    switch ($operation) {
-        case "+":
-            echo sum($arg1, $arg2);
-            break;
-        case "-":
-            echo sum($arg1, $arg2);
-            break;
-        case "*":
-            echo sum($arg1, $arg2);
-            break;
-        case "/":
-            echo sum($arg1, $arg2);
-            break;
-        default:
-        echo "ошибка";
-            break;
-    };
-};
-arithmetic(1,3,"+");
-
+$path = $_SERVER['DOCUMENT_ROOT'] . "/img/1.jpg";
+$file = fopen($path, "r");
+echo  fgets($file);
+?>
